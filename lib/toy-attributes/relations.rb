e8f1e09b4a6a9c119fa3ead9774cd56b590a2326
unknown <Mortaro@dChan.(none)>
2013-01-23 19:44:54 -0200

    adding multiple has_one support

--- lib/toy-attributes/relations.rb
+++ lib/toy-attributes/relations.rb
@@ -7,19 +7,22 @@ module ToyAttributes::Relations
       alias :_has_one :has_one
 
       def has_many *models
-        options = models.extract_options!
-        models.each do |model|
-          self._has_many model, options
-          enforce_relation_with model
-        end
+        has :many, *models
       end
 
-      def has_one model
-        self._has_one model
-        enforce_relation_with model
+      def has_one *models
+        has :one, *models
       end
 
       private
+
+      def has quantity, *models
+        options = models.extract_options!
+        models.each do |model|
+          self.send :"_has_#{quantity}", model, options
+          enforce_relation_with model
+        end
+      end
 
       def enforce_relation_with model
         related = model.to_s.singularize.classify.constantize
